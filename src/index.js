85551c297d65910f1f42f9d71a1e7c1acf512b2f
IhorNovikow <gosha.noviko2012@gmail.com>
2022-02-22 10:04:25 +0300

    fix: fix mistake with (-)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (a) {
-    return(+(String(a).split('').reverse().join('')))
+    if (a<0){
+        a=a*(-1)
+        return(+(String(a).split('').reverse().join('')))
+    }else{
+        return(+(String(a).split('').reverse().join('')))
+    }
+   
 }
